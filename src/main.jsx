fe9983e331055f155f31d88b90c77f0d4413f960
Numan Balık <numanbalik72@gmail.com>
2026-04-24 13:55:29 +0200

    update project

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "./context/theme-context.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <ThemeProvider>
     <App />
+    <Analytics />
   </ThemeProvider>,
 );
